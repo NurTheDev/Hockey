1e19b9b90bc61f5b2047a7d7710483d8ad2f37cf
nurislam <nur756.islam@gmail.com>
2024-09-18 00:01:37 +0600

    hero section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,34 @@
 module.exports = {
   content: ["*"],
   theme: {
+    fontSize: {
+      mh_22: [
+        "22px",
+        {
+          lineHeight: "1.2rem",
+          fontWeight: "800",
+        },
+      ],
+      text_progress: [
+        "20px",
+        {
+          lineHeight: "1.2rem",
+          fontWeight: "600",
+        },
+      ],
+      dh: ["36px", { lineHeight: "1.2rem", fontWeight: "800" }],
+      normal_text_size: ["16px", { lineHeight: "26px", fontWeight: "400" }],
+      progress_text: ["28px", { lineHeight: "1.2rem", fontWeight: "800" }],
+    },
     fontFamily: {
       manrope: '"Manrope", sans-serif',
     },
     extend: {
+      spacing: {
+        mx_w_m: "292px",
+        mx_w_d: "607px",
+        mx_progress: "250px",
+      },
       colors: {
         primary_color: "#FF4240",
         font_black: "#131318",
